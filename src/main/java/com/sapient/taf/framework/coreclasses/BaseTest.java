3a934fd228b8da5f7cf67cb849b9726cf3e7c3bc
SAPIENT\sgane7 <sganesh@sapient.com>
2017-11-02 08:43:04 +0530

    Added BaseTest.

---
+++ src/main/java/com/sapient/taf/framework/coreclasses/BaseTest.java
@@ -0,0 +1,11 @@
+package com.sapient.taf.framework.coreclasses;
+
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+public class BaseTest extends AbstractTestNGSpringContextTests {
+
+	public BaseTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
