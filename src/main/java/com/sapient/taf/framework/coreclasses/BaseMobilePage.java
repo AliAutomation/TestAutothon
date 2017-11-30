03fd3a283bf0738e9b0a93bc01a88fadd0c2df3f
SAPIENT\sgane7 <sganesh@sapient.com>
2017-11-30 16:12:48 +0530

    Brought the MobilePageCode as well in sync

--- src/main/java/com/sapient/taf/framework/coreclasses/BaseMobilePage.java
+++ src/main/java/com/sapient/taf/framework/coreclasses/BaseMobilePage.java
@@ -1,20 +1,27 @@
 package com.sapient.taf.framework.coreclasses;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.sapient.taf.drivermanager.DriverManager;
 import io.appium.java_client.AppiumDriver;
 
-public class BaseMobilePage implements BasePage  {
+public abstract class BaseMobilePage implements BasePage  {
 
 	protected AppiumDriver<?> driver;
+	protected Wait<? extends WebDriver> wait;
+	protected ObjectRepository objectRepository;
 
-	protected AppiumDriver<?> getDriver() {
-		return driver;
+	protected BaseMobilePage() {
+		this(DriverManager.getDriver().getMobileDriver());
 	}
-
-	protected void setDriver(AppiumDriver<?> driver) {
-		this.driver = driver;
-	}	
 	
 	protected BaseMobilePage(AppiumDriver<?> driver) {
+		this(driver, new WebDriverWait(driver, FrameworkConstants.maxWebPageOrWaitTime));
+	}
+	
+	protected BaseMobilePage(AppiumDriver<?> driver, Wait<? extends WebDriver> wait) {
 		this.driver = driver;
+		this.wait = wait;
 	}
 }
