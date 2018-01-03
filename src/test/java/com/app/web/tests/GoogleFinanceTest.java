fee7ec4b65ab9665336d8f581e96067bac7582f3
Himanshu Arora <harora15@sapient.com>
2018-01-03 17:34:57 +0530

    Added previous utils-mod

--- src/test/java/com/app/web/tests/GoogleFinanceTest.java
+++ src/test/java/com/app/web/tests/GoogleFinanceTest.java
@@ -2,40 +2,15 @@ package com.app.web.tests;
 
 import java.io.IOException;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.app.web.pages.GoogleHomePage;
-import com.sapient.taf.assertions.SoftAssertCustom;
-import com.sapient.taf.reporting.ReportUtil;
 
 public class GoogleFinanceTest {
 
-
 	@Test
-	public void searchMethodA1() throws IOException {
+	public void dummyTest() throws IOException {
 		GoogleHomePage ghp = new GoogleHomePage();
-		ReportUtil.logInfo("Test A1 - Step 1");
-		ReportUtil.logInfo("Test A1 - Step 2");
-		ReportUtil.logInfo("Test A1 - Step 3");
-		ReportUtil.logFail("Failed");
-		
 		ghp.searchText("AAPL stock");
-		ReportUtil.logInfo("Test A - Step 1");
-		
-		SoftAssertCustom.checkForErrors();
-	
 	}
-	
-	@Test
-	public void searchMethodA2() throws IOException {
-		GoogleHomePage ghp = new GoogleHomePage();
-		ReportUtil.logInfo("Test A2 - Step 1");
-		ReportUtil.logInfo("Test A2 - Step 2");
-		ReportUtil.logInfo("Test A2 - Step 3");
-		ghp.searchText("AAPL stock");
-		Assert.assertTrue(true);
-	}
-	
-	
 }
