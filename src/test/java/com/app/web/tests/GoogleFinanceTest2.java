366991f95d06acbe9e92459e3db54cd2e41be496
Himanshu Arora <harora15@sapient.com>
2018-01-03 18:10:14 +0530

    added web test cases

---
+++ src/test/java/com/app/web/tests/GoogleFinanceTest2.java
@@ -0,0 +1,31 @@
+package com.app.web.tests;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+import com.app.web.pages.GoogleHomePage;
+import com.sapient.taf.reporting.ReportUtil;
+
+public class GoogleFinanceTest2 {
+
+	@Test
+	public void searchMethod21() throws IOException {
+		GoogleHomePage ghp = new GoogleHomePage();
+		ghp.searchText("AAPL stock");
+		ReportUtil.logInfo("Test b1 - Step 1");
+		ReportUtil.logInfo("Test b1 - Step 2");
+		ReportUtil.logInfo("Test b1 - Step 3");
+		throw new SkipException("Test Exception");
+		// Reporter.
+	}
+
+	@Test
+	public void searchMethod22() throws IOException {
+		GoogleHomePage ghp = new GoogleHomePage();
+		ghp.searchText("AAPL stock");
+	}
+
+}
